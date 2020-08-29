affdc198fc690897c971741ba186d24e24b6dde4
vitkolos <vit.kolos@gmail.com>
2020-08-29 15:52:04 +0200

    unused fonts hidden

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@ export default {
     mounted() {
         // http://patorjk.com/software/taag/#p=display&f=Ivrit&t=Regenschori
         console.log("%c  ____                                _                _ \n |  _ \\ ___  __ _  ___ _ __  ___  ___| |__   ___  _ __(_)\n | |_) / _ \\/ _` |/ _ \\ '_ \\/ __|/ __| '_ \\ / _ \\| '__| |\n |  _ <  __/ (_| |  __/ | | \\__ \\ (__| | | | (_) | |  | |\n |_| \\_\\___|\\__, |\\___|_| |_|___/\\___|_| |_|\\___/|_|  |_|\n            |___/                                        ",
-        "font-family: monospace, monospace; font-weight: bold;");
+        "font-family: monospace; font-weight: bold;");
         console.log("Hmm, asi se tady vyznáš… Stále hledáme posily do našeho týmu vývojářů, takže jestli se k nám chceš přidat, neváhej nás kontaktovat na redakce@proscholy.cz.");
     }
 };
