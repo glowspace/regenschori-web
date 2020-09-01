6cb3ffed4db7e7f185f445560578a726364b00a3
vitkolos <vit.kolos@gmail.com>
2020-09-01 20:08:10 +0200

    fixed css from components

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,7 +135,8 @@ export default {
                     paths: glob.sync([
                         path.join(__dirname, './pages/**/*.vue'),
                         path.join(__dirname, './layouts/**/*.vue'),
-                        path.join(__dirname, './components/**/*.vue')
+                        path.join(__dirname, './components/**/*.vue'),
+                        path.join(__dirname, './node_modules/@bit/**/*.vue')
                     ]),
                     whitelist: ['html', 'body', 'nuxt-progress', 'tag-selected'],
                     whitelistPatterns: [/^v-((?!application).)*$/, /^nuxt/],
