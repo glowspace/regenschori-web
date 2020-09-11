5b12f9862c964a92c419efb82056ed9d266f50d7
Mira <athes01@gmail.com>
2020-09-11 13:29:09 +0200

    removed isHttps

--- middleware.js
+++ middleware.js
@@ -1,5 +1,3 @@
-import isHTTPS from 'is-https'
-
 export default function(req, res, next) {
     // detect urls you'd like to redirect
     // call res.redirect(CODE, NEWURL)
@@ -12,30 +10,6 @@ export default function(req, res, next) {
         return res.end(destination)
     }
     // www end
-
-    // https start
-    if (req.headers.host == 'www.regenschori.cz') {
-        const options = {
-            trustProxy: true,
-            redirectPort: 443,
-            redirectHost: '',
-            redirectUnknown: true,
-            statusCode: 307
-        }
-
-        const _port = options.redirectPort === 443 ? '' : (':' + options.redirectPort)
-        const url = req.url || ''
-        const _isHttps = isHTTPS(req, options.trustProxy)
-        const shouldRedirect = options.redirectUnknown ? !_isHttps : _isHttps === false
-
-        if (shouldRedirect) {
-            const _redirectHost = (options.redirectHost || req.headers.host || '').split(':')[0]
-            const ـredirectURL = 'https://' + _redirectHost + _port + url
-            res.writeHead(options.statusCode, { Location: ـredirectURL })
-            return res.end(ـredirectURL)
-        }
-    }
-    // https end
 
     next()
 }
