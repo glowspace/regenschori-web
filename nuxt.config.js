efb0fad2d41a10cb85721277866281eee23b6e5d
vitkolos <vit.kolos@gmail.com>
2020-11-13 16:38:05 +0100

    reverted liturgy recommendation wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+import PurgecssPlugin from 'purgecss-webpack-plugin'
+import glob from 'glob-all'
+import path from 'path'
 
 export default {
     ssr: !process.env.NUXT_SPA,
@@ -14,13 +17,13 @@ export default {
     head: {
         title: 'Regenschori',
         meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
-            {name: 'msapplication-TileColor', content: '#da532c'},
-            {name: 'theme-color', content: '#292929'},
+            { name: 'msapplication-TileColor', content: '#da532c' },
+            { name: 'theme-color', content: '#292929' },
 
-            {property: 'og:type', content: 'website'},
+            { property: 'og:type', content: 'website' },
             // { property: 'og:image', content: 'https://zpevnik.proscholy.cz/banner.png' },
             // { property: 'twitter:card', content: 'summary_large_image' },
             // { property: 'twitter:image', content: 'https://zpevnik.proscholy.cz/banner.png' }
@@ -41,7 +44,7 @@ export default {
             }
         ],
         script: [
-            {src: '/dark.js'}
+            { src: '/dark.js' }
         ]
     },
     messages: {
@@ -51,7 +54,7 @@ export default {
         nuxtjs: '',
         back_to_home: 'Zpět na úvodní stránku',
         server_error_details:
-            `Ajajaj, na našem serveru se někde stala chyba.
+          `Ajajaj, na našem serveru se někde stala chyba.
           <br>Zkuste <u><a href="/">použít vyhledávání</a></u>.
           <br>Chybu také můžete <u><a
           href="https://docs.google.com/forms/d/e/1FAIpQLSfry7CQD0vPpuC_VB7xGR6NUF2WdPUytQwX8KipKoZcIYxbdA/viewform?usp=pp_url&entry.1025781741=–&entry.456507920=e500"
@@ -61,53 +64,43 @@ export default {
           document.getElementById('report-link').getAttribute('href').replace('=–&', '=' + encodeURIComponent(window.location.href) + '&'));}</script>`,
         client_error: 'Chyba',
         client_error_details:
-            'Během renderování stránky došlo k chybě. Více informací najdeš v konzoli nástrojů pro vývojáře.'
+          'Během renderování stránky došlo k chybě. Více informací najdeš v konzoli nástrojů pro vývojáře.'
     },
     /*
      ** Customize the progress-bar color
      */
-    loading: {color: '#aaaaaa'},
-
+    loading: { color: '#aaa' },
     /*
      ** Global CSS
      */
-    css: ['@/assets/sass/app.scss'],
-
+    css: ['~assets/sass/app.scss'],
     /*
      ** Plugins to load before mounting the App
      */
     plugins: [
         // ssr: false to only include it on client-side
-        {src: '~/plugins/firebase.js', ssr: false},
+        { src: '~/plugins/firebase.js', ssr: false }
     ],
     /*
      ** Nuxt.js dev-modules
      */
     buildModules: [
-        ['@nuxtjs/google-analytics', {id: 'G-CTBZFXNL2E'}],
+        ['@nuxtjs/google-analytics', { id: 'G-CTBZFXNL2E' }],
         '@nuxtjs/router',
         '@nuxtjs/dotenv',
-        ['@nuxtjs/vuetify', {treeShake: true, theme: {disable: true}}],
-        ['@nuxtjs/date-fns', {locales: ['cs']}]
+        ['@nuxtjs/vuetify', { treeShake: true, theme: { disable: true } }],
+        ['@nuxtjs/date-fns', { locales: ['cs'] }]
     ],
     /*
     ** Nuxt.js modules
     */
-    modules: [
-        // '@nuxtjs/pwa',
-        'bootstrap-vue/nuxt',
-        '@nuxtjs/apollo',
-        '@nuxtjs/axios',
-        '@nuxtjs/proxy',
-        '@nuxtjs/sentry'
+   modules: [
+       // '@nuxtjs/pwa',
+       '@nuxtjs/apollo',
+       '@nuxtjs/axios',
+       '@nuxtjs/proxy',
+       '@nuxtjs/sentry'
     ],
-
-    // Bootstrap Vue SCSS resourcess are loaded manually in app.scss.
-    bootstrapVue: {
-        bootstrapCSS: false,
-        bootstrapVueCSS: false
-    },
-
     apollo: {
         clientConfigs: {
             default: {
@@ -137,25 +130,25 @@ export default {
         /*
          ** You can extend webpack config here
          */
-        // extend(config, {isDev, isClient}) {
-        //     config.plugins.push(
-        //         new PurgecssPlugin({
-        //             paths: glob.sync([
-        //                 path.join(__dirname, './pages/**/*.vue'),
-        //                 path.join(__dirname, './layouts/**/*.vue'),
-        //                 path.join(__dirname, './components/**/*.vue'),
-        //                 path.join(__dirname, './node_modules/@bit/**/*.vue')
-        //             ]),
-        //             whitelist: ['html', 'body', 'nuxt-progress', 'tag-selected'],
-        //             whitelistPatterns: [/^v-((?!application).)*$/, /^nuxt/],
-        //             whitelistPatternsChildren: [/^theme--*/, /^v-((?!application).)*$/, /^nuxt/, /data-dark/]
-        //         })
-        //     );
-        //
-        //     if (isClient) {
-        //         config.optimization.splitChunks.maxSize = 500000;
-        //     }
-        // },
-        // extractCSS: true
+        extend(config, { isDev, isClient }) {
+            config.plugins.push(
+                new PurgecssPlugin({
+                    paths: glob.sync([
+                        path.join(__dirname, './pages/**/*.vue'),
+                        path.join(__dirname, './layouts/**/*.vue'),
+                        path.join(__dirname, './components/**/*.vue'),
+                        path.join(__dirname, './node_modules/@bit/**/*.vue')
+                    ]),
+                    whitelist: ['html', 'body', 'nuxt-progress', 'tag-selected'],
+                    whitelistPatterns: [/^v-((?!application).)*$/, /^nuxt/],
+                    whitelistPatternsChildren: [/^theme--*/, /^v-((?!application).)*$/, /^nuxt/, /data-dark/]
+                })
+            );
+
+            if (isClient) {
+                config.optimization.splitChunks.maxSize = 500000;
+            }
+        },
+        extractCSS: true
     }
 };
